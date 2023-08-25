07880c67251675d8126e58d39e5d77ea19b31ab9
Hiroki Kato <hirokikato1@gmail.com>
2023-08-25 11:22:52 -0700

    completes lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,18 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(list):
+    for count, element in enumerate(list, 1):
+        print(f"{count}. {element}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(list):
+    return [f"{element.title()}!" for element in list]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(list):
+    over_four_bool_list = [len(element)>4 for element in list]
+    return any(over_four_bool_list)
 
-def find_the_cheese():
-    pass
+def find_the_cheese(list):
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    for food in list:
+        if food in cheese_types:
+            return food
+    else:
+        return None
